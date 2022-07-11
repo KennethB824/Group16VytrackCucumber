e530451cdc0fb7878715dfe57db4818581d7a661
lvayner <lvayner67@gmail.com>
2022-07-11 16:17:01 -0400

    us34  added

--- src/test/java/com/cydeo/runner/CukesRunner.java
+++ src/test/java/com/cydeo/runner/CukesRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features" ,
         glue = "com/cydeo/step_definitions",
         dryRun =false,
-        tags = "@B26G16-37"
+        tags = "@B26G16-34"
 )
 
 public class CukesRunner {
